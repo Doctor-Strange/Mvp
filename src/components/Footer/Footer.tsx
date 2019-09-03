cabbe2c04019a7ba71594fdbec509ec791a56e81
sajad saderi <saderi.sajad@gmail.com>
2019-09-03 20:58:28 +0430

    Ver 1.1.26

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -439,7 +439,7 @@ const Footer: React.FunctionComponent = ({ changeLangFunc }) => (
               <li>
                 <span>۱۳۹۸ اتولی ©</span>
               </li>
-              <li>ver : 1.1.25</li>
+              <li>ver : 1.1.26</li>
             </AdditionalLinks>
           </div>
         </div>

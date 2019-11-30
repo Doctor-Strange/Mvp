5370e2a1b4365799d3562fcf3aceca1c72e03131
sajad saderi <saderi.sajad@gmail.com>
2019-11-30 15:06:29 +0330

    Change Favicon's size to 48*84 pixels

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -129,8 +129,8 @@ export default class extends Document {
           />
 {/* 980624 */}
           <link rel="shortcut icon" href="/static/favicon.ico" />
-<link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+{/* <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/> */}
 <link rel="manifest" href="/static/site.webmanifest"/>
 <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="##4ba3ce"/>
 <meta name="msapplication-TileColor" content="#000000"/>

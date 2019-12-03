b25a5355957ac4b588e8f130120d5bca60ffd494
sajad saderi <saderi.sajad@gmail.com>
2019-12-03 11:09:02 +0330

    ver 1.5.9, UI Bug: description line break

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -2866,7 +2866,7 @@ OTILI
 //   background: #ffb7b7;
 // }
 pre{
-  white-space: inherit;
+  white-space: pre-line;
   font-size: 1rem !important;
     line-height: 1.6 !important;
     font-family: Vazir,sans-serif !important;

0d887d7774af5d8dfe5ba56cec34b6b2865f4891
sajad saderi <saderi.sajad@gmail.com>
2019-12-23 08:04:16 +0330

    ver: 1.6.12, before The Impossible Missiona

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -515,7 +515,7 @@ const Footer: React.FunctionComponent = ({ changeLangFunc }) => {
           </div> */}
           <div className="col-lg-12">
             <AdditionalLinks>
-              <span className="ver">1.6.11</span>
+              <span className="ver">1.6.12</span>
               <Link href="/site-map">
                 <a
                   style={{

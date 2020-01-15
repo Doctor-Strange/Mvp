de87a5ffb2eae3c4a2392805697ec97e72a67a5d
sajad saderi <saderi.sajad@gmail.com>
2020-01-15 13:38:22 +0330

    add limitation in get utl for sitemap

--- src/API/Get/getUrlLinkForSitemao.ts
+++ src/API/Get/getUrlLinkForSitemao.ts
@@ -4,9 +4,11 @@ const DOMAIN = process.env.PRODUCTION_ENDPOINT;
 const GET_USER = '/core/landing/list';
 
 export const REQUEST_URLS_FOR_SITE_MAP = () => {
+  console.log(DOMAIN + GET_USER);
+  
   return new Promise((resolve, reject) => {
     axios
-      .get(DOMAIN + GET_USER)
+      .get(DOMAIN + GET_USER+'?limit=900')
       .then(response => {
         // console.log("get user info", response)
         if (response.data.success) {

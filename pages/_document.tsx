4f787433c1712f06b81d00b8e8dfeb882fcff1f9
sajad saderi <saderi.sajad@gmail.com>
2020-01-28 09:28:13 +0330

    noindex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -81,8 +81,21 @@ export default class extends Document {
           heap.load("3071100507");`,
         }}>
         </script>}
+        { process.env.NODE_ENV !== 'development' ? <script  dangerouslySetInnerHTML={{
+          __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+          heap.load("329839554");`,
+        }}>
+        </script>
+        :<script  dangerouslySetInnerHTML={{
+          __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+          heap.load("3071100507");`,
+        }}>
+        </script>}
         {/* dev^ */}
-
+        { process.env.NODE_ENV === 'development ' && 
+        <meta name="robots" content="noindex"/> }
+        { process.env.NODE_ENV === 'development' && 
+        <meta name="robots" content="noindex"/> }  
 
 
         {/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147651642-1"></script>
